b8297e14796c01cc298a68e33be8c0642b27b596
github kinop1987 <kinop1987@gmail.com>
2020-07-13 16:56:29 +0900

    memo

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
 
-  #devise_scope :user do
-    #get 'login', to: 'devise/sessions#new'
-    #post 'login', to: 'devise/sessions/#create'
-    #delete 'logout', to: 'devise/sessions#destroy' 
-  #end
+  resources :users do
+    resources :memos 
+  end
 end
