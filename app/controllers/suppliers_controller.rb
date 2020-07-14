96f1b20b200ea2dda6aff21c243b2b4561971daf
github kinop1987 <kinop1987@gmail.com>
2020-07-14 16:43:53 +0900

    supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,14 +1,21 @@
 class SuppliersController < ApplicationController
+
   def index
   end
+
   def new
   end
+
   def create
   end
+
   def edit
   def
+
   def update
   end
-  def destroy
-  end
 end
+end
+
+
+
